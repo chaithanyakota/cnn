ac835e21f43cad88da1d5d97e2f9052c11ac3f71
ChaithanyaKota <chaithanya.kota007@gmail.com>
2024-04-06 10:38:07 -0500

    backprop

--- conv.py
+++ conv.py
@@ -24,6 +24,7 @@ class Conv3x3:
         '''
             - input is a 2d numpy array
         '''
+        self.last_input = input
         h, w = input.shape
         output = np.zeros((h - 2, w - 2, self.num_filters))
 
@@ -31,3 +32,24 @@ class Conv3x3:
             output[i, j] = np.sum(im_region * self.filters, axis=(1, 2))
 
         return output
+    
+    def backprop(self, d_L_d_out, learn_rate):
+        '''
+        Performs a backward pass of the conv layer.
+        - d_L_d_out is the loss gradient for this layer's outputs.
+        - learn_rate is a float.
+        '''
+        d_L_d_filters = np.zeros(self.filters.shape)
+
+        for im_region, i, j in self.iterate_regions(self.last_input):
+            for f in range(self.num_filters):
+                d_L_d_filters[f] += d_L_d_out[i, j, f] * im_region
+
+        # Update filters
+        self.filters -= learn_rate * d_L_d_filters
+
+        # We aren't returning anything here since we use Conv3x3 as
+        # the first layer in our CNN. Otherwise, we'd need to return
+        # the loss gradient for this layer's inputs, just like every
+        # other layer in our CNN.
+        return None
